f3f7ec7ac350174f5c1385c23641c575001c3ac0
Gammir <miroslav.kamenar@gmail.com>
2022-01-25 16:28:49 +0100

    TowerBase

---
+++ Assets/Miro/TowerBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour
+{
+    public int health;
+    public int damage;
+    public int cost;
+    public float attackSpeed;
+
+    public void Attack()
+    {
+
+    }
+}
