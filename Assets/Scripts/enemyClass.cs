c426c4d4a23df845bf99bbeb69ebfb9c488e4109
Erikendo <erik.terenyi123@gmail.com>
2022-03-28 18:13:42 +0200

    Final
    sdasd

--- Assets/Scripts/enemyClass.cs
+++ Assets/Scripts/enemyClass.cs
@@ -7,25 +7,45 @@ public class enemyClass : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 1f;
     public LayerMask towerLayer;
-
+    public int attackDamage = 2;
+    public float walkSpeed;
+    public bool isMoving;
+    public bool inRange;
+    public Rigidbody2D rb;
     public Animator animator;
+    public float timeStamp;
+    public float coolDownInSeconds = 5f;
+    private void Start() {
+        isMoving = true;
+        inRange = false;
+    }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(isMoving)
         {
+            rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        }
+        if(inRange)
+        {
+            isMoving = false;
+            rb.velocity = new Vector2(0, 0);
             Attack();
         }
     }
-    void Attack()
+    public virtual void Attack()
     {
-        animator.SetTrigger("Attack");
-
-        Collider2D[] hitTowers = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, towerLayer);
-
-        foreach(Collider2D tower in hitTowers)
+        if(timeStamp <= Time.time)
         {
-            Debug.Log("We hit " + tower.name);
-        }
+            animator.SetTrigger("Attack");
+
+            Collider2D[] hitTowers = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, towerLayer);
+
+            foreach(Collider2D tower in hitTowers)
+            {
+                tower.GetComponent<towerrr>().TakeDamage(attackDamage);
+            }
+            timeStamp = Time.time + coolDownInSeconds;
+        }    
     }
     void OnDrawGizmosSelected() 
     {
@@ -33,5 +53,20 @@ public class enemyClass : MonoBehaviour
         return;
 
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+    private void OnTriggerStay2D(Collider2D trig) 
+    {
+        if(trig.gameObject.tag == "Tower")
+        {
+            inRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D trig) 
+    {
+        if(trig.gameObject.tag == "Tower")
+        {
+            inRange = false;
+            isMoving = true;
+        }
     }
 }
--- Assets/Scripts/towerrr.cs
+++ Assets/Scripts/towerrr.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class towerMovement : MonoBehaviour
+public class towerrr : MonoBehaviour
 {
+    public int maxHealth = 10;
+    public int currentHealth;
     private Rigidbody2D rb2D;
     float moveSpeed;
     Vector2 move;
     void Start()
     {
+        currentHealth = maxHealth;
         rb2D = gameObject.GetComponent<Rigidbody2D>();
         moveSpeed = 200f;
     }
@@ -24,4 +27,24 @@ public class towerMovement : MonoBehaviour
         rb2D.AddForce(move * moveSpeed * Time.deltaTime);
     }
 
+    public void TakeDamage(int damage)
+    {
+        currentHealth-= damage;
+
+        //animacka
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Debug.Log("Tower ded");
+
+        //animacka
+
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }   
 }
